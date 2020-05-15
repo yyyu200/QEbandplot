2c825fbf6fdb42dc459a2671b13f0d0a791737d2
yyyu200 <yyyu200@163.com>
2020-05-15 08:46:50 +0800

    add function, warning

--- pw_band_plot.py
+++ pw_band_plot.py
@@ -17,7 +17,7 @@ def parse_filband(feig, npl=10):
     nbnd=int(l.split(',')[0].split('=')[1])
     nks=int(l.split(',')[1].split('=')[1].split('/')[0])
     
-    eig=np.zeros((nks,nbnd),dtype=float)
+    eig=np.zeros((nks,nbnd),dtype=np.float32)
     for i in range(nks):
         l=feig.readline()
         count=0
@@ -36,10 +36,9 @@ def parse_filband(feig, npl=10):
     return eig, nbnd, nks
 
 do_find_gap=True
+e_ref=0.0 # set to fermi-level in scf output for metal, only applicable for do_find_gap=False
 if do_find_gap:
-    nvband=26 # valence band number, for insulators only
-else:
-    e_ref=0.0 # set to fermi-level in scf output for metal, use with do_find_gap=False
+    nvband=26 # valence band number, only applicable for do_find_gap=True
 
 ymin=-10  # y range in plot
 ymax=10
@@ -52,22 +51,28 @@ F=plt.gcf()
 
 eig, nbnd, nks=parse_filband('bd.dat')
 
+if nbnd < nvband:
+    print("warning: only partial valence bands are plotted, increase nvband.")
+elif nbnd == nvband:
+    print("warning: only valence bands are plotted, increase nvband.")
+
+
 plt.xlim([0,nks-1]) # k-points
 plt.ylim([ymin,ymax])
 #plt.xlabel(r'$k (\AA^{-1})$',fontsize=16)
 plt.ylabel(r' E (eV) ',fontsize=16)
 
-if do_find_gap:
+if do_find_gap and nbnd > nvband: # for insulators only, nvband can be found by gappw.sh(https://github.com/yyyu200/gappw)
     eig_vbm=max(eig[:,nvband-1])
     eig_cbm=min(eig[:,nvband])
     Gap=eig_cbm-eig_vbm
-    plt.title("Band gap= %.4f eV" % (Gap))  # for insulators only
-    e_ref=eig_vbm
+    plt.title("Band gap= %.4f eV" % (Gap))  
+    e_ref=eig_vbm 
 
 for i in range(nbnd):
     line1=plt.plot( eig[:,i]-e_ref,color='r',linewidth=lw ) 
 
-vlines= np.arange(0,nks,20) # positions of vertial lines
+vlines= np.arange(0,nks,20) # positions of vertical lines, or specified by [0, 20, 40, ...]
 for vline in vlines:
     plt.axvline(x=vline, ymin=ymin, ymax=ymax,linewidth=lw,color='black')
 
