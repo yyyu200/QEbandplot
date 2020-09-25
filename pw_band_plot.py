e0bc6fb25f37cf080b7fc4c69ecc10497bf17177
yyyu200 <yyyu200@163.com>
2020-09-25 11:47:26 +0800

    modify at 20200925-11:47:26-di

--- pw_band_plot.py
+++ pw_band_plot.py
@@ -7,38 +7,49 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
 def parse_filband(feig, npl=10):
     # feig : filband in bands.x input file
-    # npl : number per line
+    # npl : number per line, 10 for bands.x, 6 for phonon
 
-    feig=open(feig)
-    l=feig.readline()
-    nbnd=int(l.split(',')[0].split('=')[1])
-    nks=int(l.split(',')[1].split('=')[1].split('/')[0])
-    
-    eig=np.zeros((nks,nbnd),dtype=np.float32)
-    for i in range(nks):
-        l=feig.readline()
-        count=0
-        if nbnd%npl==0:
-            n=nbnd//npl
+    f=open(feig,'r')
+    lines = f.readlines()
+
+    header = lines[0].strip()
+    line = header.strip('\n')
+    shape = re.split('[,=/]', line)
+    nbnd = int(shape[1])
+    nks = int(shape[3])
+    eig = np.zeros((nks, nbnd+1), dtype=np.float32)
+
+    dividend = nbnd
+    divisor = npl
+    div = nbnd // npl + 1 if nbnd % npl == 0 else nbnd // npl + 2 
+    kinfo=[]
+    for index, value in enumerate(lines[1:]):
+        value = value.strip(' \n')
+        quotient = index // div
+        remainder = index % div
+
+        if remainder == 0:
+            kinfo.append(value)
         else:
-            n=nbnd//npl+1
-        for j in range(n):
-            l=feig.readline()
-            for k in range(len(l.split())):
-                eig[i][count]=l.split()[k]  # str to float
-                count=count+1
-                
-    feig.close()
+            value = re.split('[ ]+', value)
+            a = (remainder - 1) * npl
+            b = a + len(value)
+            eig[quotient][a:b] = value
 
-    return eig, nbnd, nks
+    f.close()
+
+    return eig, nbnd, nks, kinfo
 
 do_find_gap=True
 e_ref=0.0 # set to fermi-level in scf output for metal, only applicable for do_find_gap=False
 if do_find_gap:
     nvband=26 # valence band number, only applicable for do_find_gap=True
+else:
+    nvband=0
 
 ymin=-10  # y range in plot
 ymax=10
@@ -49,7 +60,7 @@ p1=plt.subplot(1, 1, 1)
 F=plt.gcf()
 #F.set_size_inches([5,5])
 
-eig, nbnd, nks=parse_filband('bd.dat')
+eig, nbnd, nks, kinfo = parse_filband('bd.dat')
 
 if nbnd <= nvband:
     print("warning: nvband should be less than the calculated band.")
@@ -76,7 +87,8 @@ for vline in vlines:
 plt.xticks( vlines, (r'${\Gamma}$', 'X', 'M', r'${\Gamma}$', 'Z',
            'R','A','Z','X','R','M','A') )
 
-plt.text(0.0, 8, '$SnO_{2}$ rutile', fontsize=12, color='black')
+plt.text(4, 8, '$SnO_{2}$ rutile', fontsize=12, color='black', bbox=dict(facecolor='white',alpha=0.99,edgecolor='black') )
+plt.tight_layout()
 
 plt.savefig('pwband.png',dpi=500)
 
